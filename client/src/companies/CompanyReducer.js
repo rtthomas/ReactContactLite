ab590b49fdf1f8cea3ca1c57e13fe0b3849245f8
rtthomas <git@robert-thomas.ca>
2020-10-14 19:32:45 -0700

    Added use of reducerHelper

--- client/src/companies/CompanyReducer.js
+++ client/src/companies/CompanyReducer.js
@@ -2,6 +2,7 @@
  * This reducer deals with all states related to companies
  */
 import * as actions from './CompanyActions';
+import { storeAll, storeOne } from '../utilities/reducerHelper';
 
 const initialState = {
     companies: [],
@@ -10,49 +11,9 @@ const initialState = {
 
 const companyReducer = (state = initialState, action) => {
     switch (action.type){
-        case actions.STORE_ALL : return storeAll(action.companies, state);
-        case actions.SAVE_AND_STORE : return storeOne(action.data, state);
+        case actions.STORE_ALL : return storeAll(state, 'companies', 'companiesMap', action.companies);
+        case actions.SAVE_AND_STORE : return storeOne(state, action.data.company, action.data.rowIndex, 'companies', 'companiesMap');
         default: return state;
-    }
-}
-
-const storeAll = (companies, state) => {
-    const newState = {...state}
-    let companiesMap = state.companiesMap
-    
-    if (!companiesMap){
-        // Companies have just been retrieved from server
-        newState.companiesMap = companies.reduce((map, company) => {
-            map[company._id] = company
-            return map
-        }, {})
-    }
-    newState.companies = companies;
-
-    return newState
-}
-
-/**
- * Stores a new or modified company. The 
- * @param {*} company 
- * @param {*} state 
- */
-const storeOne = ({company, rowIndex}, state) => {
-    // Add or replace it in the map
-    const companies = [...state.companies];
-    if (rowIndex == null){
-        companies.push(company)
-    }
-    else {    
-        companies[rowIndex] = company
-    }
-    const companiesMap = {...state.companiesMap}
-    companiesMap[company._id] = company;
-
-    return {
-        ...state,
-        companies,
-        companiesMap
     }
 }
 
