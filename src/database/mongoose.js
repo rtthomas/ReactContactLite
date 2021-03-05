88c329b482d9bc02b67b8fa255f62349bede408b
EC2 Default User <ec2-user@ip-172-31-12-89.us-west-2.compute.internal>
2021-03-05 03:45:46 +0000

    Mongoose cleanup

--- src/database/mongoose.js
+++ src/database/mongoose.js
@@ -3,13 +3,14 @@ const mongoose = require('mongoose')
 // This will prevent deprecation warning re Server Discover and Monitoring engine
 mongoose.set('useUnifiedTopology', true);
 
-
 const accessKeyId = encodeURIComponent("AKIA4BYFC2EVYW4QNUOX");
 const secretAccessKey = encodeURIComponent("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");
 const clusterUrl = "cluster0.vpqlb.mongodb.net/test";
+
+const connectionUrl = `mongodb+srv://${accessKeyId}:${secretAccessKey}@${clusterUrl}`
+
 const authMechanism = "MONGODB-AWS";
 const authSource = "$external"
-const connectionUrl = `mongodb+srv://${accessKeyId}:${secretAccessKey}@${clusterUrl}`
 
 console.log('Connecting to ' + connectionUrl)
 mongoose.connect(connectionUrl, {
@@ -20,8 +21,3 @@ mongoose.connect(connectionUrl, {
     authSource
 })
 
-// mongoose.connect(process.env.MONGODB_URL, {
-//     useNewUrlParser: true,
-//     useCreateIndex: true,
-//     useFindAndModify: false
-// })
