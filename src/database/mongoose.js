63dad2801e8dcdaa0cfbc3be02f9df08cef8854c
EC2 Default User <ec2-user@ip-172-31-12-89.us-west-2.compute.internal>
2021-03-05 03:33:52 +0000

    Connection fix for mongodb atlas

--- src/database/mongoose.js
+++ src/database/mongoose.js
@@ -3,8 +3,25 @@ const mongoose = require('mongoose')
 // This will prevent deprecation warning re Server Discover and Monitoring engine
 mongoose.set('useUnifiedTopology', true);
 
-mongoose.connect(process.env.MONGODB_URL, {
+
+const accessKeyId = encodeURIComponent("AKIA4BYFC2EVYW4QNUOX");
+const secretAccessKey = encodeURIComponent("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");
+const clusterUrl = "cluster0.vpqlb.mongodb.net/test";
+const authMechanism = "MONGODB-AWS";
+const authSource = "$external"
+const connectionUrl = `mongodb+srv://${accessKeyId}:${secretAccessKey}@${clusterUrl}`
+
+console.log('Connecting to ' + connectionUrl)
+mongoose.connect(connectionUrl, {
     useNewUrlParser: true,
     useCreateIndex: true,
-    useFindAndModify: false
+    useFindAndModify: false,
+    authMechanism,
+    authSource
 })
+
+// mongoose.connect(process.env.MONGODB_URL, {
+//     useNewUrlParser: true,
+//     useCreateIndex: true,
+//     useFindAndModify: false
+// })
