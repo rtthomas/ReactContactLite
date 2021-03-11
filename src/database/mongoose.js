aec159241bb239c48f10cbcec0a8296af4a334c3
rtthomas <git@robert-thomas.ca>
2021-03-11 02:27:43 +0000

    Added try/catch block

--- src/database/mongoose.js
+++ src/database/mongoose.js
@@ -3,8 +3,8 @@ const mongoose = require('mongoose')
 // This will prevent deprecation warning re Server Discover and Monitoring engine
 mongoose.set('useUnifiedTopology', true);
 
-const accessKeyId = encodeURIComponent("");
-const secretAccessKey = encodeURIComponent("");
+const accessKeyId = encodeURIComponent(process.env.KEY_ID);
+const secretAccessKey = encodeURIComponent(process.env.ACCESS_KEY);
 const clusterUrl = "cluster0.vpqlb.mongodb.net/test";
 
 const connectionUrl = `mongodb+srv://${accessKeyId}:${secretAccessKey}@${clusterUrl}`
@@ -13,11 +13,18 @@ const authMechanism = "MONGODB-AWS";
 const authSource = "$external"
 
 console.log('Connecting to ' + connectionUrl)
-mongoose.connect(connectionUrl, {
-    useNewUrlParser: true,
-    useCreateIndex: true,
-    useFindAndModify: false,
-    authMechanism,
-    authSource
-})
+
+try {
+    mongoose.connect(connectionUrl, {
+        useNewUrlParser: true,
+        useCreateIndex: true,
+        useFindAndModify: false,
+        authMechanism,
+        authSource
+    })
+    console.log("Connected to database")
+}
+catch (error){
+    console.log("Mongo connection:" + error)
+}
 
