554fd570ec7d567e6cc268b97bbdb76e467ec010
rtthomas <git@robert-thomas.ca>
2021-03-11 18:53:30 -0800

    Mongo connection config

--- src/database/mongoose.js
+++ src/database/mongoose.js
@@ -3,25 +3,34 @@ const mongoose = require('mongoose')
 // This will prevent deprecation warning re Server Discover and Monitoring engine
 mongoose.set('useUnifiedTopology', true);
 
-const accessKeyId = encodeURIComponent(process.env.KEY_ID);
-const secretAccessKey = encodeURIComponent(process.env.ACCESS_KEY);
-const clusterUrl = "cluster0.vpqlb.mongodb.net/test";
+const options = {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false
+}
+let connectionUrl;
 
-const connectionUrl = `mongodb+srv://${accessKeyId}:${secretAccessKey}@${clusterUrl}`
-
-const authMechanism = "MONGODB-AWS";
-const authSource = "$external"
-
-console.log('Connecting to ' + connectionUrl)
+if (process.env.KEY_ID){
+    // Running in AWS . DB is in Atlas (cloud) 
+    const accessKeyId = encodeURIComponent(process.env.KEY_ID);
+    const secretAccessKey = encodeURIComponent(process.env.ACCESS_KEY);
+    const clusterUrl = "cluster0.vpqlb.mongodb.net/test";
+    
+    connectionUrl = `mongodb+srv://${accessKeyId}:${secretAccessKey}@${clusterUrl}`
+    
+    options.authMechanism = "MONGODB-AWS";
+    options.authSource = "$external"
+    
+    console.log('Connecting to ' + clusterUrl)
+}
+else {
+    // Running in local enviroenment
+    connectionUrl = process.env.MONGODB_URL
+    console.log('Connecting to ' + connectionUrl)
+}
 
 try {
-    mongoose.connect(connectionUrl, {
-        useNewUrlParser: true,
-        useCreateIndex: true,
-        useFindAndModify: false,
-        authMechanism,
-        authSource
-    })
+    mongoose.connect(connectionUrl, options);
     console.log("Connected to database")
 }
 catch (error){
