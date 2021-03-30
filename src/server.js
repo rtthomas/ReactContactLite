6f781b084c538007a118b5f808f3c8f0264d54e3
rtthomas <git@robert-thomas.ca>
2021-03-30 01:19:42 +0000

    Suffix router names with 'Router'

--- src/server.js
+++ src/server.js
@@ -3,12 +3,12 @@ const path = require('path')
 
 require('./database/mongoose')
 
-const appointmentRouter = require('./routers/appointment')
-const companyRouter = require('./routers/company')
-const contactRouter = require('./routers/contact')
-const emailRouter = require('./routers/email')
-const personRouter = require('./routers/person')
-const positionRouter = require('./routers/position')
+const appointmentRouter = require('./routers/appointmentRouter')
+const companyRouter = require('./routers/companyRouter')
+const contactRouter = require('./routers/contactRouter')
+const emailRouter = require('./routers/emailRouter')
+const personRouter = require('./routers/personRouter')
+const positionRouter = require('./routers/positionRouter')
 
 const app = express()
 const port = process.env.PORT || 5000
@@ -18,7 +18,10 @@ const public = path.join(__dirname, '../client/build')
 app.use(express.static(public))
 
 // Translate request body from JSON
-app.use(express.json())
+app.use(express.json({type: [
+    'text/plain',           // Used by all messages from SNS
+    'application/json'      // For future use
+]}))
 
 app.use(appointmentRouter)
 app.use(companyRouter)
