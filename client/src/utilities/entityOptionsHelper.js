71846fd8eeb817961073fb58e6d71876c68f9c1b
rtthomas <git@robert-thomas.ca>
2021-07-29 15:27:29 -0700

    Created

---
+++ client/src/utilities/entityOptionsHelper.js
@@ -0,0 +1,17 @@
+/**
+ * Assembles the set of selector option sets
+ * @param {array} optionDefinitions array of {entityList, type, mappedAttribute}
+ */
+const buildOptionSets = (optionDefinitions) => {
+    const optionSets = {}
+    optionDefinitions.forEach(({entityList, type, mappedAttribute}) => {
+        let options = []
+        entityList.forEach(entity => {
+            options.push({ label: entity[mappedAttribute], value: entity._id })
+        })
+        optionSets[type] = options
+    })
+    return optionSets
+}
+
+export default buildOptionSets;
