cb7dcb23f65622260313324bbda919f764a4569e
rtthomas <git@robert-thomas.ca>
2022-01-14 12:52:54 -0800

    Added saveEmail to support hiding

--- client/src/emails/EmailActions.js
+++ client/src/emails/EmailActions.js
@@ -1,5 +1,22 @@
 /**
  * Actions associated with emails
  */
+import axios from 'axios';
+
 /******* Synchronous actions *********/
 export const STORE_ALL = 'STORE_ALL_EMAILS';
+export const SAVE_AND_STORE = 'SAVE_AND_STORE_EMAILS'; 
+
+/******* Asynchronous actions *********/
+/**
+ * Sends an new updated email entity to the server then dispatches to the store
+ * @param {*} email 
+ */
+export const saveEmail = ((email, rowIndex) => {
+    return dispatch => {
+        axios.put('/emails/' + email._id, email)
+        .then((response) => {
+            dispatch({ type: SAVE_AND_STORE, data: {email: response.data, rowIndex} })
+        });        
+    }    
+})
