288d0ba5e88714dccc767b7dcdcdde312f7a6ec4
rtthomas <git@robert-thomas.ca>
2022-02-05 12:39:25 -0800

    Added STORE_ONE processing

--- client/src/emails/EmailReducer.js
+++ client/src/emails/EmailReducer.js
@@ -2,7 +2,7 @@
  * This reducer deals with all states related to emails
  */
 import * as actions from './EmailActions';
-import { storeAll } from '../utilities/reducerHelper';
+import { storeAll, storeOne } from '../utilities/reducerHelper';
 
 const initialState = {
     emails: [],
@@ -12,6 +12,7 @@ const initialState = {
 const emailReducer = (state = initialState, action) => {
     switch (action.type){
         case actions.STORE_ALL : return storeAll(state, 'emails', 'emailsMap', action.emails);
+        case actions.STORE_ONE : return storeOne(state, action.data.email, action.data.rowIndex, 'emails', 'emailsMap');
         default: return state;
     }
 }
