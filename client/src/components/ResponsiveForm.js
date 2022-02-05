29229bb54d8092cf2dfcb533eaf1706087c07bf8
rtthomas <git@robert-thomas.ca>
2022-02-05 13:06:58 -0800

    fieldType, fieldWidth constants relocated

--- client/src/components/ResponsiveForm.js
+++ client/src/components/ResponsiveForm.js
@@ -1,5 +1,6 @@
 /**
- * A responsive form component for creating, viewing or editing an entity, displayed as a modal popup.
+ * A responsive form component for creating, viewing or editing an entity, 
+ * displayed as a modal popup.
  */
 import React, { useState, useMemo } from 'react'
 import styled from 'styled-components'
@@ -8,27 +9,8 @@ import 'react-datepicker/dist/react-datepicker.css'
 import { Modal, Button } from 'react-bootstrap'
 import Selector from './Selector'
 import TextArea from './TextArea'
+import { fieldType } from './Constants'
 
-export const fieldType = {
-    TEXT:           'TEXT',         // Specifies HTML input element of type 'text'
-    TEXT_AREA:      'TEXT_AREA',    // Specifies HTML textarea element
-    DATE_TIME:      'DATE_TIME',    // Specifies DatePicker with time select
-    DATE:           'DATE',         // Specifies DatePicker without time select
-    URL:            'URL',          // Specifies a link element
-    SELECT_ENTITY:  'SELECT_ENTITY',// Specifies a selector displaying a set of entities
-    SELECT:         'SELECT',       // Specifies a set of options
-    EMAIL:          'EMAIL',        // Valid only if readOnly
-    ATTACHMENT:     'ATTACHMENT',   // Valid only if readOnly TODO
-    BOOLEAN_HIDDEN: 'BOOLEAN_HIDDEN'// Not to be displayed in the form; it is displayed in <ResponsiveTable> as a checkbox 
-}
-
-// Display widths to apply to fields in the table view; not relevant for the form
-// The values i.e. 1, 2, 3 will become the 'flex-grow' CSS attribute 
-export const fieldWidth = {
-    NARROW: 1,
-    NORMAL: 2,
-    WIDE:   3
-}
 /**
  * Generates the component
  * @param {object}     theEntity    an entity to be viewed or edited, or null
@@ -94,10 +76,6 @@ function ResponsiveForm({
             entity[e.name] = e.value
         }
         setEntity({ ...entity })
-    }
-
-    function expandTextArea(e) {
-        console.log(e);
     }
 
     const title = readOnly
@@ -354,7 +332,7 @@ const StyledFieldWithoutLabel = styled(Field)`
 `
 
 const formatEmailObject = (emailArray) => {
-    if (emailArray.length == 0){
+    if (emailArray.length === 0){
         return null
     }
     const email = emailArray[0]
